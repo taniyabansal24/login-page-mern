809d92fe31825faff6101920a1f807e62bc62cd4
Taniya Bansal <149322176+taniyabansal24@users.noreply.github.com>
2024-11-04 12:28:00 +0530

    Update Signup.jsx

--- frontent/src/components/Signup.jsx
+++ frontent/src/components/Signup.jsx
@@ -144,7 +144,7 @@ const Signup = () => {
                 setTimeout(() => handleFormSubmit(e),0);
                 setTimeout(() => {
                     navigate('/login')
-                }, 0)
+                }, 1000)
             } else if (error) {
                 const details = error?.details[0].message;
                 handleError(details);
